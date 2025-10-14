65b392d617c3ba844cc92b34ad8143cc041f8cbd
iubou <114062398+xkusnier@users.noreply.github.com>
2025-10-14 21:14:29 +0200

    Update and rename dashboard.js to dashboard.html

--- static/js/dashboard.js
+++
@@ -1,8 +0,0 @@
-async function loadData() {
-  const response = await fetch("/api/data/latest");
-  const data = await response.json();
-  console.log(data);
-  // TODO: vykresliť graf Chart.js
-}
-
-loadData();
